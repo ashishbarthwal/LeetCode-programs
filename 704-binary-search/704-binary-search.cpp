24d0b6b3d227ca58e9709920cc4847142c03b6e2
Ashish Barthwal <ashishbarthwal1000@gmail.com>
2022-07-27 21:25:28 +0530

    Time: 66 ms (32.41%), Space: 27.6 MB (63.76%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid,i=0,n=nums.size();
+        int l=0,r=n-1;
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            if(target<nums[mid])
+            {
+                r=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else 
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
