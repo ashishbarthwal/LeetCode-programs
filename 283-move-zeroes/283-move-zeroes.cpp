c87286bd8e041dcbf5619a98eac8798cc88777ea
Ashish Barthwal <ashishbarthwal1000@gmail.com>
2022-08-08 21:18:22 +0530

    Time: 24 ms (90.66%), Space: 19.3 MB (13.34%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+          int n=nums.size();
+        int i,count=0,x=0;
+        if(n==1)
+        {
+            return;
+        }
+        else{
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[x++]=nums[i];
+            }
+            else
+                count++;
+        }
+        for(i=n-count;i<n;i++)
+        {
+          nums[i]=0;
+        }
+        return;
+    }
+  }
+};
