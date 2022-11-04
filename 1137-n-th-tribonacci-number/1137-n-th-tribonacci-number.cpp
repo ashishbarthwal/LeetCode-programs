9976b0b023a44b063593649cad0448e760f1c4e6
Ashish Barthwal <ashishbarthwal1000@gmail.com>
2022-11-04 23:50:11 +0530

    Time: 3 ms (35.65%), Space: 6 MB (52.98%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int a=0,b=1,c=1,x=0;
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
-        else if(n==2)
-            return 1;
-        else
+        int f[n+3];
+        f[0]=0;
+        f[1]=1;
+        f[2]=1;
         for(int i=3;i<=n;i++)
-        {   
-            x=a+b+c;
-            a=b;
-            b=c;
-            c=x;
+        {
+            f[i]=f[i-3]+f[i-2]+f[i-1];
         }
-        return x;
+        return f[n];
     }
 };
